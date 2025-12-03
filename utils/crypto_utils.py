5ca14058b51bd31ad29ac9a2e1f96d3125b71325
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 22:07:41 +0800

    feat(utils): add sha256 function

--- utils/crypto_utils.py
+++ utils/crypto_utils.py
@@ -5,3 +5,8 @@ import hashlib
 def md5(s: str) -> str:
     """MD5哈希"""
     return hashlib.md5(s.encode()).hexdigest()
+
+
+def sha256(s: str) -> str:
+    """SHA256哈希"""
+    return hashlib.sha256(s.encode()).hexdigest()
