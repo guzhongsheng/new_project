966a5d79c20b0d772af872f2b23f0536bf2cd99d
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 22:09:31 +0800

    feat(utils): add from_json function

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -5,3 +5,8 @@ import json
 def to_json(obj) -> str:
     """对象转JSON字符串"""
     return json.dumps(obj, ensure_ascii=False)
+
+
+def from_json(s: str):
+    """JSON字符串转对象"""
+    return json.loads(s)
