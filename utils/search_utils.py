e411bdafa29349e7ae73190038f31b7f0821f206
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 22:14:14 +0800

    feat(utils): add binary_search function

--- utils/search_utils.py
+++ utils/search_utils.py
@@ -7,3 +7,17 @@ def linear_search(lst: list, target) -> int:
         if item == target:
             return i
     return -1
+
+
+def binary_search(lst: list, target) -> int:
+    """二分搜索（要求列表已排序）"""
+    left, right = 0, len(lst) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if lst[mid] == target:
+            return mid
+        elif lst[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
