899e404bc8fd618998691d8f2d15783084eb3719
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 22:11:46 +0800

    feat(utils): add insertion_sort function

--- utils/sort_utils.py
+++ utils/sort_utils.py
@@ -21,3 +21,16 @@ def quick_sort(lst: list) -> list:
     middle = [x for x in lst if x == pivot]
     right = [x for x in lst if x > pivot]
     return quick_sort(left) + middle + quick_sort(right)
+
+
+def insertion_sort(lst: list) -> list:
+    """插入排序"""
+    arr = lst.copy()
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
