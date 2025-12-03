745b4c298060dbfb47f81d44c0f7b87af24629ca
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 22:12:10 +0800

    feat(utils): add selection_sort function

--- utils/sort_utils.py
+++ utils/sort_utils.py
@@ -34,3 +34,15 @@ def insertion_sort(lst: list) -> list:
             j -= 1
         arr[j + 1] = key
     return arr
+
+
+def selection_sort(lst: list) -> list:
+    """选择排序"""
+    arr = lst.copy()
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
