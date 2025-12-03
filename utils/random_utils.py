91b02dd1efc65f8a177cf433f19437ed4479ac6a
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 21:58:44 +0800

    feat(utils): add random_float function

--- utils/random_utils.py
+++ utils/random_utils.py
@@ -5,3 +5,8 @@ import random
 def random_int(a: int, b: int) -> int:
     """生成随机整数"""
     return random.randint(a, b)
+
+
+def random_float(a: float, b: float) -> float:
+    """生成随机浮点数"""
+    return random.uniform(a, b)
