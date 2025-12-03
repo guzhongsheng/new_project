419cd38b02e26951c30643befc0690bdce5874ab
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 22:01:35 +0800

    feat(utils): add is_phone function

--- utils/validation_utils.py
+++ utils/validation_utils.py
@@ -6,3 +6,9 @@ def is_email(s: str) -> bool:
     """验证邮箱格式"""
     pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     return bool(re.match(pattern, s))
+
+
+def is_phone(s: str) -> bool:
+    """验证手机号格式"""
+    pattern = r'^1[3-9]\d{9}$'
+    return bool(re.match(pattern, s))
