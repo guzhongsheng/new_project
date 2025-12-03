cdfe8a78164b7ade500b9d1ec869815969eeff4c
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 21:52:57 +0800

    feat(utils): add dir_exists function

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -5,3 +5,8 @@ import os
 def file_exists(path: str) -> bool:
     """判断文件是否存在"""
     return os.path.isfile(path)
+
+
+def dir_exists(path: str) -> bool:
+    """判断目录是否存在"""
+    return os.path.isdir(path)
