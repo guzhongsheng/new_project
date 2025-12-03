100fa965a731375375bce6a159ea86e47df438ab
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 21:45:41 +0800

    feat(utils): add today function

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -5,3 +5,8 @@ from datetime import datetime
 def now() -> str:
     """获取当前时间"""
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def today() -> str:
+    """获取今天日期"""
+    return datetime.now().strftime("%Y-%m-%d")
