c35939459904527a85118d0c69b5824584e96cd8
guzhongsheng <2023204038@mail.nwpu.edu.cn>
2025-12-03 22:22:20 +0800

    feat(utils): add version and more exports

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,7 @@
 from .math_utils import add, subtract, multiply, divide, power, modulo, absolute
 from .string_utils import reverse, uppercase, lowercase, capitalize, count_chars, is_palindrome
 from .list_utils import sum_list, max_list, min_list, avg_list, reverse_list, unique_list, flatten
+from .date_utils import now, today, year, month, day, weekday, is_leap_year
+from .counter import Counter
+
+__version__ = "1.0.0"
